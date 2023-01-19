fc1568d3947d2ab5aa1f49376052efaa7ad8d3d5
Satej Balaso Patil <112341637+satejbpatil@users.noreply.github.com>
2023-01-19 22:32:57 +0530

    Link: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/

---
+++ subtract_the_product_and_sum_of_digits_of_an_integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int m=1;
+        int a;
+        int s=0;
+        while(n!=0){
+            a=n%10;
+            m=m*a;
+            s=s+a;
+            n=n/10;
+        }
+        int ans=m-s;
+        return ans;
+    }
+};
