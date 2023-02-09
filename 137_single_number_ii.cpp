654937704270a27fb49031cc31ae99b0c45276a3
Satej Balaso Patil <112341637+satejbpatil@users.noreply.github.com>
2023-02-09 23:45:41 +0530

    Link: https://leetcode.com/problems/single-number-ii/

---
+++ 137_single_number_ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& v) {
+        map<int, int>m;
+        int l;
+        for(auto i:v){
+            m[i]++;
+        }
+        v.clear();
+        for(auto &i:m){
+            if(i.second==1){
+                l=i.first;
+            }
+        }
+    return l;
+    }
+};
