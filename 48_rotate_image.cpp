3a19187ee0cbe66a1c494a19298e71559bda2298
Satej Balaso Patil <112341637+satejbpatil@users.noreply.github.com>
2023-03-03 23:17:31 +0530

    Link

---
+++ 48_rotate_image.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& v) {
+        vector<vector<int>> ans;
+        vector<int> z;
+        int a=v.size()-1;
+        int b=v.size()-1;
+        while(a!=-1 && b!=-1){
+            z.push_back(v[a][b]);
+            a--;
+            if(a==-1){
+                ans.push_back(z);
+                z.clear();
+                b--;
+                a=v.size()-1;
+            }
+            }
+            v.clear();
+            v=ans;
+            reverse(v.begin(),v.end());
+    }
+};
